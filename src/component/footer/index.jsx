8c58416462af3cee2b747ec26c7c6509d93fac6f
RyanMustofa <rm281003@gmail.com>
2020-05-14 18:28:52 +0700

    first commit

---
+++ src/component/footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => {
+    return(
+        <div className="container">
+            <div className="row justify-content-center" style={{ marginTop: "150px" }}>
+                <div className="col-6 text-center">
+                    copyright 2020 ryan mustofa
+                </div>
+            </div>
+        </div>
+    )
+}
